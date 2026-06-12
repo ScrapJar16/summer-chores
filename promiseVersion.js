11b078884a951bedc156396f999d0ba411513e8c
ScrapJar16 <mpeayjarman@gmail.com>
2026-06-12 16:24:51 -0400

    adding version for promises

---
+++ promiseVersion.js
@@ -0,0 +1,79 @@
+
+function mowYard(name){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`${name} mowed the yard.`);
+        }, 2000);
+    });
+    
+};
+
+function weedEat(name, callback) {
+    return new Promise ((resolve,reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.01) {
+            resolve(`${name} finished using the weed eater.`);
+            }
+            else {reject(`${name} fell asleep after mowing the yard.`)
+            };
+        }, 1500);
+    });
+        
+};
+
+function trimHedges(name) {
+    return new Promise ((resolve,reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.05) {
+                resolve(`${name} finished trimming the hedges.`);
+            } else {
+                reject(`${name} fell asleep after weed eating the yard.`);
+            }
+        }, 1000);
+    });
+    
+    
+};
+
+function collectWood(name) {
+    return new Promise ((resolve,reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.1) {
+                resolve(`${name} finished collecting wood.`);
+            } else {
+                reject(`${name} fell asleep after trimming the hedges.`);
+            }
+        }, 2500);
+    })
+    
+ 
+};
+
+function waterGarden(name) {
+    return new Promise ((resolve,reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.15) {
+                resolve(`${name} finished watering the garden.`);
+            }
+            else {
+                reject(`${name} fell asleep after collecting wood.`);
+            }
+        }, 500); 
+    });
+};
+
+function doSummerChores(name) {
+    try {
+      mowYard(name) 
+      .then(value => {console.log(value); return weedEat(name)})
+      .then(value => {console.log(value); return trimHedges(name)})
+      .then(value => {console.log(value); return collectWood(name)})
+      .then(value => {console.log(value); return waterGarden(name)})
+      .then(value => {console.log(`${name} finished all their chores!`)});  
+    }
+    catch(error) {
+        console.error('an error occured:', error);
+    }            
+};
+
+doSummerChores("Madilyn");
